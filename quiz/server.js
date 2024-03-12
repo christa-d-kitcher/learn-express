0e5dc262b955685f60055d3e534ad16cef456041
Christa D. Kitcher <christad.kitcher@Christas-MacBook-Pro.local>
2024-03-12 18:02:48 -0400

    Commit for last few express activities

--- quiz/server.js
+++ quiz/server.js
@@ -29,8 +29,17 @@ app.use(
   cors({origin: 'http://localhost:3000'})
 );
 app.use('/read/usernames', addMsgToRequest);
+app.use('/read/usernames/:name', addMsgToRequest);
+
 
 app.get('/read/usernames', (req, res) => {
+  let usernames = req.users.map(function(user) {
+    return {id: user.id, username: user.username};
+  });
+  res.send(usernames);
+});
+
+app.get('/read/usernames/:name', (req, res) => {
   let usernames = req.users.map(function(user) {
     return {id: user.id, username: user.username};
   });
--- quiz/writeUsers.js
+++ quiz/writeUsers.js
@@ -2,3 +2,10 @@ const express = require('express')
 const fs = require('fs');
 const path = require('path');
 const router = express.Router()
+
+
+
+
+
+module.exports = router
+
